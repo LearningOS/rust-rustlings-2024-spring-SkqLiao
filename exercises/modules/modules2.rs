85f0a08d15775bf30676769ce349e92bb32365f9
Skqliao <skqliao@gmail.com>
2024-04-16 10:51:22 +0800

    update

--- exercises/modules/modules2.rs
+++ exercises/modules/modules2.rs
@@ -9,8 +9,8 @@
 
 mod delicious_snacks {
     // TODO: Fix these use statements
-    use self::fruits::PEAR;
-    use self::veggies::CUCUMBER;
+    pub use self::fruits::PEAR as fruit;
+    pub use self::veggies::CARROT as veggie;
 
     mod fruits {
         pub const PEAR: &'static str = "Pear";
--- exercises/strings/strings4.rs
+++ exercises/strings/strings4.rs
@@ -20,9 +20,9 @@ fn main() {
     string(String::from("hi"));
     string("rust is fun!".to_owned());
     string("nice weather".into());
-    string_slice(format!("Interpolation {}", "Station"));
+    string(format!("Interpolation {}", "Station"));
     string_slice(&String::from("abc")[0..1]);
-    string("  hello there ".trim());
+    string_slice("  hello there ".trim());
     string("Happy Monday!".to_string().replace("Mon", "Tues"));
     string("mY sHiFt KeY iS sTiCkY".to_lowercase());
 }

b5b827fb12790359555bf2df9dbea2da56035b2e
Skqliao <skqliao@gmail.com>
2024-04-22 00:18:55 +0800

    update

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,23 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+        let (name, age) = s.split_once(",").unwrap_or(("", ""));
+        if name.len() == 0 {
+            return Person::default();
+        }
+        let age = match age.parse::<usize>() {
+            Ok(age) => age,
+            Err(_) => return Person::default(),
+        };
+        Person {
+            name: name.to_string(),
+            age,
+        }
     }
 }
 
